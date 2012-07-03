46071ad267ea091688d1aa8e2da751c6b1aabfb8
maxexcloo <max@excloo.com>
2012-07-03 21:34:03 +1000

    Moved HTTP Install Extra List To Config Array

--- modules/http-install-extra.sh
+++ modules/http-install-extra.sh
@@ -24,11 +24,23 @@ fi
 # Install Packages
 subheader "Installing Extra Packages..."
 
-# Loop Through Package List
-while read package; do
+# Define Packages
+PACKAGELIST=$(read_var_module packages),
+
+# Loop Through Packages
+while echo $PACKAGELIST | grep \, &> /dev/null; do
+	# Define Current Package
+	FILE=${PACKAGELIST%%\,*}
+
+	# Remove Current Package From List
+	PACKAGELIST=${PACKAGELIST#*\,}
+
 	# Install Currently Selected Package
-	package_install $package
-done < $MODULEPATH/$MODULE/$DISTRIBUTION
+	package_install $FILE
+done
+
+# Unset Array
+unset PACKAGELIST
 
 # Check Package
 if check_package "php5-fpm"; then
