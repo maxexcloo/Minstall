3f0ab1088eb2c7d59b2a0b91d54655e0f3439d54
maxexcloo <max@excloo.com>
2012-08-03 21:07:26 +1000

    Checked Changes

--- modules/clean-common.sh
+++ modules/clean-common.sh
@@ -8,7 +8,7 @@ if check_package "nginx"; then
 
 	# Update HTTP Config
 	cp -rf $MODULEPATH/http-install-nginx/nginx/{*.conf,fastcgi_params} /etc/nginx/ > /dev/null 2>&1
-	cp -rf $MODULEPATH/http-install-nginx/nginx/conf.d/* /etc/nginx/conf.d/ > /dev/null 2>&1
+	cp -rf $MODULEPATH/http-install-nginx/nginx/conf.d/*.conf /etc/nginx/conf.d/ > /dev/null 2>&1
 	mv -f /etc/nginx/mime.types /etc/nginx/nginx.d/mime.conf > /dev/null 2>&1
 	rm -rf /etc/nginx/sites-* > /dev/null 2>&1
 fi
@@ -16,7 +16,7 @@ fi
 # Check Package
 if check_package "php5-fpm"; then
 	# Update PHP Config
+	cp -rf $MODULEPATH/http-install-php/php5/conf.d/*.conf /etc/php5/fpm/conf.d/ > /dev/null 2>&1
 	cp -rf $MODULEPATH/http-install-php/php5/fpm/*.conf /etc/php5/fpm/ > /dev/null 2>&1
-	cp -rf $MODULEPATH/http-install-php/php5/fpm/conf.d/* /etc/php5/fpm/conf.d/ > /dev/null 2>&1
 	rm -rf /etc/php5/fpm/pool.d/www.conf > /dev/null 2>&1
 fi
