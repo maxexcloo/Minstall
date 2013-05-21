d14b559936d058f130495d97c1a1e91f916a2241
Max Schaefer <max@excloo.com>
2013-05-21 21:37:47 +1000

    Updated Configure Nginx

--- modules/configure-http-nginx/init.sh
+++ modules/configure-http-nginx/init.sh
@@ -4,29 +4,6 @@
 # Check Package
 check_package_message "" "nginx" "install-http-nginx"
 
-# Enable Cache
-if question --default yes "Do you want to enable cache support and create a cache directory? (Y/n)" || [ $(read_variable_module cache) = 1 ]; then
-	subheader "Enabling Cache Support..."
-	cp -r $MODULEPATH/install-http-nginx/etc/nginx.d/cache.conf /etc/nginx/nginx.d/
-	mkdir -p /var/lib/nginx/cache
-	chown -R www-data:www-data /var/lib/nginx/cache
-# Disable Cache
-else
-	subheader "Disabling Cache Support..."
-	rm /etc/nginx/nginx.d/cache.conf &> /dev/null
-	rm -rf /var/lib/nginx/cache &> /dev/null
-fi
-
-# Enable Caching SSL Sessions
-if question --default yes "Do you want to enable caching of SSL sessions (can increase responsiveness over SSL)? (Y/n)" || [ $(read_variable_module cache_ssl) = 1 ]; then
-	subheader "Enabling SSL Session Caching..."
-	cp -r $MODULEPATH/install-http-nginx/etc/nginx.d/cache_ssl.conf /etc/nginx/nginx.d/
-# Disable Caching SSL Sessions
-else
-	subheader "Disabling SSL Session Caching..."
-	rm /etc/nginx/nginx.d/cache_ssl.conf &> /dev/null
-fi
-
 # Enable Compression
 if question --default yes "Do you want to enable gzip compression to save bandwidth and decrease page load time (compresses CSS, HTML, Javascript & XML at gzip compression level 6)? (Y/n)" || [ $(read_variable_module gzip) = 1 ]; then
 	subheader "Enabling Compression..."
@@ -38,13 +15,36 @@ else
 fi
 
 # Enable Virtual Host For Hostname
-if question --default yes "Do you want to enable a virtual host that accepts all requests for the servers hostname (can be useful for scripts such as bandwidth monitors or web administration)? (Y/n)" || [ $(read_variable_module hostname_virtual_host) = 1 ]; then
+if question --default yes "Do you want to enable a virtual host that accepts all requests for the servers hostname (can be useful for scripts such as bandwidth monitors)? (Y/n)" || [ $(read_variable_module hostname_virtual_host) = 1 ]; then
 	subheader "Enabling Virtual Host For Hostname..."
 	mv /etc/nginx/hosts.d/system.conf.disabled /etc/nginx/hosts.d/system.conf
 # Disable Virtual Host For Hostname
 else
 	subheader "Disabling Virtual Host For Hostname..."
 	mv /etc/nginx/hosts.d/system.conf /etc/nginx/hosts.d/system.conf.disabled
+fi
+
+# Enable Proxy Cache
+if question --default yes "Do you want to enable proxy cache support and create a cache directory? (Y/n)" || [ $(read_variable_module proxy_cache) = 1 ]; then
+	subheader "Enabling Proxy Cache Support..."
+	cp -r $MODULEPATH/install-http-nginx/etc/nginx.d/proxy_cache.conf /etc/nginx/nginx.d/
+	mkdir -p /var/lib/nginx/cache
+	chown -R www-data:www-data /var/lib/nginx/cache
+# Disable Proxy Cache
+else
+	subheader "Disabling Proxy Cache Support..."
+	rm /etc/nginx/nginx.d/proxy_cache.conf &> /dev/null
+	rm -rf /var/lib/nginx/cache &> /dev/null
+fi
+
+# Enable SSL Session Cache
+if question --default yes "Do you want to enable caching of SSL sessions (can increase responsiveness over SSL)? (Y/n)" || [ $(read_variable_module ssl_session_cache) = 1 ]; then
+	subheader "Enabling SSL Session Cache..."
+	cp -r $MODULEPATH/install-http-nginx/etc/nginx.d/ssl_session_cache.conf /etc/nginx/nginx.d/
+# Disable SSL Session Cache
+else
+	subheader "Disabling SSL Session Cache..."
+	rm /etc/nginx/nginx.d/ssl_session_cache.conf &> /dev/null
 fi
 
 # Enable Default Host Protection
