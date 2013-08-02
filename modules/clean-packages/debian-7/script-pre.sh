dfd2905b385768e64be7630564aa54a2db721857
Max Schaefer <max@excloo.com>
2013-08-02 23:10:26 +1000

    Added Some Debian 7 Fixes

--- modules/clean-packages/debian-7/script-pre.sh
+++ modules/clean-packages/debian-7/script-pre.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 # Pre Clean Commands (Debian 7)
 
-# Debian 7 DPKG Inport Bug Hack
+# Enable x32 Support On x64 Systems
+if [ $ARCHITECTURE = "x64" ]; then
+	dpkg --add-architecture i386
+fi
+
+# Remove Sendmail
+package_remove sendmail-base
+
+# TEMP: Create Hack Package List
 string_replace_output "temp.packages.list" "temp.packages.hack" " install" ""
-cat temp.packages.hack | while read line; do package_install $line; done
+
+# TEMP: Iterate Through Package Listed
+cat temp.packages.hack | while read line; do
+	if ! check_package $line; then
+		package_install $line
+	fi
+done
