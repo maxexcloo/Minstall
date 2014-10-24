db204fa6b27a9b621a698d68204413988666e875
thesupremecommander <thesupremecmdr@gmail.com>
2014-10-24 13:36:41 -0400

    Update the packages and install the HTTPS transport before adding the repo.

--- modules/install-extra-repositories/debian-7/nodejs.sh
+++ modules/install-extra-repositories/debian-7/nodejs.sh
@@ -4,15 +4,18 @@
 # Ask If Repository Should Be Installed
 if question --default yes "Do you want to install the Node repository? (Y/n)" || [ $UNATTENDED = 1 ]; then
     subheader "Installing Node.js Repository..."
+	
+	# Update Packages
+	package_update
+
+    # Install HTTPS Transport
+    package_install "apt-transport-https"
 
     # Add Repository Key
     repo_key "https://deb.nodesource.com/gpgkey/nodesource.gpg.key"
 
     # Add Repository
     repo_add "nodejs" "deb https://deb.nodesource.com/node wheezy main"
-
-    # Install HTTPS Transport
-    package_install "apt-transport-https"
 
     # Add HTTPS Transport To Package List
     echo "apt-transport-https" >> $MODULEPATH/clean-packages/custom/custom
